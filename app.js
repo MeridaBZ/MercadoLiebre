4fcd8a9c58568a32669961a65659622849cf155a
MeridaBZ <marianablanez@gmail.com>
2022-08-16 01:02:18 -0300

    Se suben los faltantes de los trabajos practicos

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const port = 3000;
 app.use(express.static(path.resolve(__dirname,'public')));
 
 app.get('/',(req, res) => res.sendFile(path.resolve(__dirname,'views', 'index.html')));
-
-
+app.get('/login', (req,res)=> res.sendFile(path.join(__dirname,'views','login.html')));
+app.get('/register', (req,res)=>res.sendFile(path.join(__dirname,'views','register.html')));
 app.listen(port, () => console.log(`Se levanto con exito el servidor en http://localhost:${port}`));
+
+
+
